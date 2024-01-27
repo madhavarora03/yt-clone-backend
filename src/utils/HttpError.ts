89ab1c41754de2ce6d061a2700129ac2bdc6fc00
madhav <eragon.cube@outlook.com>
2024-01-28 02:06:57 +0530

    Adding logger utility and creating custom error handler

---
+++ src/utils/HttpError.ts
@@ -0,0 +1,29 @@
+class HttpError extends Error {
+  public statusCode: number;
+  public errors: any[];
+  public success: boolean;
+  public isOperational: boolean;
+
+  constructor(
+    statusCode: number,
+    message = 'Something went wrong',
+    errors = [],
+    isOperational = true,
+    stack = '',
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.message = message;
+    this.errors = errors;
+    this.isOperational = isOperational;
+    this.success = false;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export default HttpError;
---
+++ src/utils/logger.ts
@@ -0,0 +1,28 @@
+import winston from 'winston';
+import { NODE_ENV } from '@/config';
+
+const enumerateErrorFormat = winston.format((info) => {
+  if (info instanceof Error) {
+    Object.assign(info, { message: info.stack });
+  }
+  return info;
+});
+
+const logger = winston.createLogger({
+  level: NODE_ENV === 'development' ? 'debug' : 'info',
+  format: winston.format.combine(
+    enumerateErrorFormat(),
+    NODE_ENV === 'development'
+      ? winston.format.colorize()
+      : winston.format.uncolorize(),
+    winston.format.splat(),
+    winston.format.printf(({ level, message }) => `${level}: ${message}`),
+  ),
+  transports: [
+    new winston.transports.Console({
+      stderrLevels: ['error'],
+    }),
+  ],
+});
+
+export default logger;
