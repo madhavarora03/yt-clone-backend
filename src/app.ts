2be2147cc84e5cd9b57deb7e48c3e3352260ba85
madhav <eragon.cube@outlook.com>
2024-01-28 09:20:57 +0530

    Add CORS support and update eslint configuration

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,22 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import { CORS_ORIGIN } from './config';
 
 const app = express();
+
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+
+// enable cors
+app.use(
+  cors({
+    origin: CORS_ORIGIN,
+    credentials: true,
+  }),
+);
+
+// cookie parser
+app.use(cookieParser());
 
 export default app;
--- src/config/index.ts
+++ src/config/index.ts
@@ -7,8 +7,9 @@ const schema = object({
   PORT: string({ required_error: 'PORT is required' }).min(1),
   ADDRESS: string().default('0.0.0.0'),
   MONGODB_URI: string({ required_error: 'MONGODB_URI is required' }).min(1),
+  CORS_ORIGIN: string({ required_error: 'CORS_ORIGIN is required' }).min(1),
 });
 
 const env = schema.parse(process.env);
 
-export const { NODE_ENV, PORT, ADDRESS, MONGODB_URI } = env;
+export const { NODE_ENV, PORT, ADDRESS, MONGODB_URI, CORS_ORIGIN } = env;
