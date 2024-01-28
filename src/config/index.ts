4746e548414c70cf559d56c6db03881363350bfb
madhav <eragon.cube@outlook.com>
2024-01-28 19:55:38 +0530

    Update token expiry validation in config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,11 +9,15 @@ const schema = object({
   ACCESS_TOKEN_SECRET: string({
     required_error: 'ACCESS_TOKEN_SECRET is required',
   }).min(1),
-  ACCESS_TOKEN_EXPIRY: string().default('1d'),
+  ACCESS_TOKEN_EXPIRY: string({
+    required_error: 'ACCESS_TOKEN_EXPIRY is required',
+  }).min(1),
   REFRESH_TOKEN_SECRET: string({
     required_error: 'REFRESH_TOKEN_SECRET is required',
   }).min(1),
-  REFRESH_TOKEN_EXPIRY: string().default('10d'),
+  REFRESH_TOKEN_EXPIRY: string({
+    required_error: 'REFRESH_TOKEN_EXPIRY is required',
+  }).min(1),
   MONGODB_URI: string({ required_error: 'MONGODB_URI is required' }).min(1),
   CORS_ORIGIN: string({ required_error: 'CORS_ORIGIN is required' }).min(1),
   AWS_ACCESS_KEY_ID: string({
