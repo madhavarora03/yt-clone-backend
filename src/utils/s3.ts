2891953ccfdfbd6b68844e75b3cc86f0453a657e
madhav <eragon.cube@outlook.com>
2024-01-28 21:35:00 +0530

    Add putObjectUrl function to handle uploading files to S3

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -1,4 +1,8 @@
-import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import {
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { AWS_S3_BUCKET_NAME } from '@/constants';
 import { AWS_ACCESS_KEY_ID, AWS_REGION, AWS_SECRET_ACCESS_KEY } from '@/config';
@@ -17,6 +21,23 @@ export async function getObjectUrl(Key: string) {
     Key,
   });
 
-  const url = await getSignedUrl(s3Client, getObjCommand);
+  const url = await getSignedUrl(s3Client, getObjCommand, {
+    expiresIn: Infinity,
+  });
+  return url;
+}
+
+export async function putObjectUrl(
+  username: string,
+  filename: string,
+  contentType: string,
+) {
+  const putObjCommand = new PutObjectCommand({
+    Bucket: AWS_S3_BUCKET_NAME,
+    Key: `uploads/${username}/${contentType.includes('video') ? 'video/' + filename : 'profile/' + filename}`,
+    ContentType: contentType,
+  });
+
+  const url = await getSignedUrl(s3Client, putObjCommand);
   return url;
 }
