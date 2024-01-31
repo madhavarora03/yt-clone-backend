5e3b7e4a9a4277a2ec5315f8f7ccb9f1cfa731e6
madhav <eragon.cube@outlook.com>
2024-01-31 10:39:11 +0530

    Add static file serving and multer middleware

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.static('public'));
 
 // enable cors
 app.use(
