df602af9765292f0f9119f05e4db85acb09f6659
madhav <eragon.cube@outlook.com>
2024-02-04 13:23:00 +0530

    Remove static file serving middleware and multer

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,6 @@ const app = express();
 
 app.use(express.json({ limit: '16kb' }));
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
-app.use(express.static('public'));
 
 // enable cors
 app.use(
